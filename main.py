5e14870eb6d3dfc208ee5dc4589a4de7ec6f07a8
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-09 08:31:51 -0700

    Add file_map for image filenames in memory game
    Added a file_map dictionary to associate numbers with image filenames.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import time
 
+file_map = {
+    1: "EARTH.png",
+    2: "ELEPHANT.png",
+    3: "FISH.png",
+    4: "HUMAN.png",
+    5: "ROBOT.png",
+    6: "SHARK.png",
+    7: "SUN.png",
+    8: "TRAIN.png",
+    9: "TREE.png",
+}
+
+# available_numbers = list(file_map.keys())
+
+# random_numbers = random.sample(available_numbers, 3)
+
+# chosen_files = [file_map[num] for num in random_numbers]
+
 st.title("The Memory Game")
 st.subheader("literally just a memory game")
 st.text("just memorize cards and you'll be good")
