e6b5553fb12be66fadd763d23d97000c0b803887
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-09 08:16:53 -0700

    Add title and difficulty buttons to memory game

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import streamlit as st
 import time
 
+st.title("The Memory Game")
+st.subtitle("literally just a memory game")
+st.text("just memorize cards and you'll be good")
+c1, c2, c3 = st.columns(3)
+easy = c1.button("Easy", icon=":material/chess_pawn:", width="stretch")
+medium = c2.button("Medium", icon=":material/chess_knight:", width="stretch")
+hard = c3.button("Hard", icon=":material/chess_king:", width="stretch")
