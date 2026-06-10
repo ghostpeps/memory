65d0dda81a5badd7a8280248f09c68e5e0678080
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-10 11:39:58 -0700

    Increase image width to 1000 pixels

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ for file in chosen_files:
     col1, col2 = st.columns([1, 2])
     
     with col1:
-        st.image(file, width=450)
+        st.image(file, width=1000)
         
     with col2:
         st.subheader("Image Title")
