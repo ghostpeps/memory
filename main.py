535e8324a23cde58f257949d1cb9a19e5f661182
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-10 12:25:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -78,3 +78,5 @@ if st.session_state.difficulty_chosen == True:
         elif s != 1:
             countdown_placeholder.subheader(f"You have {s} seconds left to memorize your cards.")
         time.sleep(1)
+        if s == 0:
+            break
