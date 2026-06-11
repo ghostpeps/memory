beebc0faa7d269a790023f3de608e43b90a1e97b
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-10 21:36:22 -0700

    Fix layout and button functionality in Memory Game

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ chosen_files = [file_map[num] for num in random_numbers]
 st.title("The Memory Game")
 st.subheader("literally just a memory game")
 st.text("just memorize the cards and you'll be good")
+
 cards = 0
 c1, c2, c3 = st.columns(3)
 easy = c1.button("Easy", on_click=select_difficulty, icon=":material/chess_pawn:", disabled=st.session_state.difficulty_chosen, width="stretch")
@@ -48,6 +49,7 @@ if hard:
     available_numbers = list(file_map.keys())
     random_numbers = random.sample(available_numbers, 7)
     chosen_files = [file_map[num] for num in random_numbers]
+
 if cards == 3:
     col1, col2, col3 = st.columns(cards)
     col1.image(chosen_files[0])
@@ -69,7 +71,6 @@ elif cards == 7:
     col5.image(chosen_files[4])
     col6.image(chosen_files[5])
     col7.image(chosen_files[6])
-
 
 if st.session_state.difficulty_chosen == True: 
     countdown_placeholder = st.empty()
