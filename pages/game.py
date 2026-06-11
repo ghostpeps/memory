c8a369499c643f4bb01f3c470747c60e3b892c5e
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-10 21:26:40 -0700

    Update game.py

--- pages/game.py
+++ pages/game.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from main import file_map
 
-chosen_files = st.session_state["chosen_files"]
-
-# 50% of the way there!! omg!!!
+if "chosen_files" not in st.session_state:
+    st.switch_page("main.py")
+else:
+    chosen_files = st.session_state["chosen_files"]
