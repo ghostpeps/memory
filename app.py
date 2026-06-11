d089ea55006e3667562c62feaa0b1b1832401c13
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-10 21:34:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 main_page = st.Page("main.py", title="Memory Game", icon=":material/home:")
-game_page = st.Page("pages/game.py", title="Memory Game", icon="🎮")
+game_page = st.Page("pages/game.py", title="Memory Game", icon=":material/playing_cards:")
 
 pg = st.navigation([main_page, game_page], position="hidden")
 pg.run()
