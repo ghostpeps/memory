b292d00a303b37302a8332fbed2ac4e11d4e4c4d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-11 10:32:46 -0700

    Update game.py

--- pages/game.py
+++ pages/game.py
@@ -35,7 +35,7 @@ c1.markdown(f"""
 
 c2.markdown("The Memory Game", text_alignment="justify")
 
-st.image(file_map[random.randint(1, 9)], width=60)
+st.image(file_map[random.randint(1, 9)], width=180)
 
 if st.button("Lose a life", on_click=remove_life):
     pass
