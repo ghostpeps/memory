fed77d4f425aad261733ddf2fa25a152607edaad
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-11 09:54:02 -0700

    Update game.py

--- pages/game.py
+++ pages/game.py
@@ -5,3 +5,23 @@ if "chosen_files" not in st.session_state:
     st.switch_page("main.py")
 else:
     chosen_files = st.session_state["chosen_files"]
+
+
+if "lives" not in st.session_state:
+    st.session_state.lives = 3
+
+def remove_life():
+    if st.session_state.lives > 0:
+        st.session_state.lives -= 1
+
+heart_display = ""
+for i in range(3):
+    if i < st.session_state.lives:
+        heart_display += ":red[:material/favorite:] "
+    else:
+        heart_display += ":red[:material/heart_broken:] "
+
+st.markdown(heart_display)
+
+if st.button("Lose a life", on_click=remove_life):
+    pass
