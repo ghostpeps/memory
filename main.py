1d93598142f6c1c6804383b23496b9b5ab5a193b
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-10 21:20:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -79,4 +79,6 @@ if st.session_state.difficulty_chosen == True:
         elif s != 1:
             countdown_placeholder.subheader(f"You have {s} seconds left to memorize your cards.")
         time.sleep(1)
+    st.session_state["chosen_files"] = chosen_files
+    st.session_state["cards"] = cards
     st.switch_page("pages/game.py")
