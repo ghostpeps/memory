124f2f1800d72f8275da7781a37dbbc56a23886d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-11 20:37:29 -0700

    Add end page to navigation in app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 main_page = st.Page("main.py", title="Memory Game", icon=":material/home:")
 game_page = st.Page("pages/game.py", title="Memory Game", icon=":material/playing_cards:")
+end_page = st.Page("pages/end.py", title="Memory Game", icon=":material/bar_chart:")
 
-pg = st.navigation([main_page, game_page], position="hidden")
+pg = st.navigation([main_page, game_page, end_page], position="hidden")
 pg.run()
