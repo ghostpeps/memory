cf910ed23254f3032a74faab96d53564c2d93fb5
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-06-11 20:43:00 -0700

    Update end.py

--- pages/end.py
+++ pages/end.py
@@ -1,2 +1,4 @@
 import streamlit as st
 
+st.write("Cards you said were NOT in the deck:", st.session_state.marked_not_in_deck)
+st.write("Cards you said were IN the deck:", st.session_state.marked_in_deck)
